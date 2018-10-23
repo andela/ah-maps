bb5c0020a383d8439e268a35b2b228425f0f50d5
Mirriam <mirrmaina@gmail.com>
2018-10-23 13:04:48 +0300

    [Chore #161255232] Add host to environment

--- authors/settings.py
+++ authors/settings.py
@@ -89,6 +89,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('HOST'),
     }
 }
 
