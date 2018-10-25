5e5ec956cebcf2ec1c54c9e8d80b50dcb2b33886
Mirr77 <mirrmaina@gmail.com>
2018-10-25 16:11:53 +0300

    [Chore #161255220]Add test for user login

--- authors/apps/authentication/test/test_user_api.py
+++ authors/apps/authentication/test/test_user_api.py
@@ -23,8 +23,16 @@ class UserTest(TestCase):
             }
         }
         self.create_url = reverse(self.namespace + ':register')
+        self.login_url = reverse(self.namespace + ':login')
        
 
     def test_create_user_api(self):
         response = self.client.post(self.create_url, self.body, format='json')
         self.assertEqual(201, response.status_code)
+
+    def test_user_login(self):
+        self.client.post(self.create_url, self.body, format='json')
+        response = self.client.post(self.login_url, self.body, format='json')
+        self.assertEqual(200, response.status_code)
+            
+
