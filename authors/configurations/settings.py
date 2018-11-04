93c7ef633c09837c57f94f99c1b536b8c97b1684
Mirr77 <mirrmaina@gmail.com>
2018-11-04 10:21:20 +0300

    (chore)Reorder social pipeline functions & add associate_email

--- authors/configurations/settings.py
+++ authors/configurations/settings.py
@@ -199,13 +199,12 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.social_uid',
     'social_core.pipeline.social_auth.social_user',
     'social_core.pipeline.user.get_username',
+    'social_core.pipeline.social_auth.associate_by_email',
     'social_core.pipeline.user.create_user',
     'social_core.pipeline.social_auth.associate_user',
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
-    'social_core.pipeline.social_auth.associate_by_email',
 )
-
 
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
