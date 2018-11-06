917baadcc5814bf2c45965216d84ab9bbd77c3f0
Mirr77 <mirrmaina@gmail.com>
2018-11-06 09:58:17 +0300

    (feat)(documentation):Document API using swagger -Install django rest swagger -Create view for rendering swagger schema -Refactor request data to remove user object -Refactor tests to use user object
    [Finishes #161255237]

--- authors/apps/authentication/test/test_user_api.py
+++ authors/apps/authentication/test/test_user_api.py
@@ -20,32 +20,43 @@ class UserTest(TestCase):
 
         self.namespace = 'authentication'
         self.body = {
-            "user": {
                 'username': faker.first_name(),
                 'email': faker.email(),
                 'password': faker.password()
             }
-        }
 
         self.user_body = {
-            'user': {
                 'username': self.user.username,
                 'email': self.user.email,
                 'password': '1234abcd'
-            }
         }
         self.not_exist= {
-             "user": {
                 'username': faker.first_name(),
                 'email': faker.email(),
                 'password': faker.password()
             }
-        }
 
-        self.no_username= self.body.update({'username':''})
-        self.no_email= self.body.update({'email':''})
-        self.email_format= self.body.update({'email':'emailformat'})
-        self.password_length= self.body.update({'password':'pass'})
+        self.no_email= {
+                'username': faker.first_name(),
+                'email': '',
+                'password': faker.password()
+            }
+        self.no_username= {
+                'username': '',
+                'email': faker.email(),
+                'password': faker.password()
+
+        }
+        self.email_format= {
+                'username': faker.first_name(),
+                'email': 'emailformat',
+                'password': faker.password()
+            }
+        self.password_length= {
+                'username': faker.first_name(),
+                'email': 'emailformat',
+                'password': 'pass'
+            }
         self.token = 'token'
 
         self.create_url = reverse(self.namespace + ':register')
@@ -59,7 +70,6 @@ class UserTest(TestCase):
 
     def test_retrieve_logged_in_user(self):
         response = self.client.get(self.retrieve_user_url)
-
         self.assertEqual(200, response.status_code)
 
     def test_update_user_api(self):
@@ -68,12 +78,12 @@ class UserTest(TestCase):
 
     def test_update_user_without_password_field(self):
         body = self.user_body
-        body.update({'user': {'password': None}})
+        body.update({'password': None})
         response = self.client.put(self.update_user_url, body, format='json')
         self.assertEqual('Please provide a password', response.data.get('message'))
 
     def test_reset_password_with_previous_password(self):
-        body = {'password': self.user_body.get('user').get('password')}
+        body = {'password': self.user_body.get('password')}
         response = self.client.put(self.update_user_url, body, format='json')
         self.assertEqual('Your new password can\'t be the same as your old password', response.data.get('message'))
 
@@ -84,7 +94,7 @@ class UserTest(TestCase):
         response4 = self.client.post(self.create_url, self.no_email, format='json')
         response5 = self.client.post(self.create_url, self.email_format, format='json')
         response6 = self.client.post(self.create_url, self.password_length, format='json')
-
+     
         self.assertEqual(201, response.status_code)
         self.assertEqual(400, response2.status_code)
         self.assertEqual(400, response3.status_code)
@@ -107,7 +117,7 @@ class UserTest(TestCase):
 
     def test_activate_user(self):
         self.client.post(self.create_url, self.user_body, format='json')
-        user = get_user_model().objects.get(email=self.user_body.get('user').get('email'))
+        user = get_user_model().objects.get(email=self.user_body.get('email'))
         self.activate_url = reverse(self.namespace + ':activate', kwargs={'token': user.token})
         activate = self.client.get(self.activate_url)
 
@@ -122,8 +132,8 @@ class UserTest(TestCase):
     
     def test_resend_activation_email(self):
         register = self.client.post(self.create_url, self.user_body, format='json')
-        activate = self.client.post(self.resend_activation_url, data={"email":self.user_body.get('user').get('email')}, head={"Content-Type":"application/json"})
-        activate_wrong_email_format = self.client.post(self.resend_activation_url, data={"email": "wrong_email_format"}, head={"Content-Type":"application/json"})
+        activate = self.client.post(self.resend_activation_url, data={"email":self.user_body.get('email')}, head={"Content-Type":"application/json"})
+        activate_wrong_email_format = self.client.post(self.resend_activation_url, data=self.email_format, head={"Content-Type":"application/json"})
         unregistered_email = self.client.post(self.resend_activation_url, data={"email": "unregistered@gmail.com"}, head={"Content-Type":"application/json"})
         self.assertEqual(activate.json().get('user').get('message'), 'Success, an activation link has been re-sent to your email.')
         self.assertEqual(activate.status_code, 200)
@@ -135,6 +145,6 @@ class UserTest(TestCase):
 
     def test_reset_password(self):
         register = self.client.post(self.create_url, self.user_body, format='json')
-        activate = self.client.post(self.reset_url, data={"email":self.user_body.get('user').get('email')}, head={"Content-Type":"application/json"})
+        activate = self.client.post(self.reset_url, data={"email":self.user_body.get('email')}, head={"Content-Type":"application/json"})
         self.assertEqual(activate.json().get('user').get('message'), 'An email has been sent to your account')
         self.assertEqual(activate.status_code, 200)
--- release.sh
+++ release.sh
@@ -1,4 +1,5 @@
 python manage.py makemigrations authentication
 python manage.py makemigrations profile
+python manage.py makemigrations article
 python manage.py makemigrations
 python manage.py migrate
