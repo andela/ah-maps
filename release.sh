f16e551bd907e26022271da486186713f5c3cd72
Mirr77 <mirrmaina@gmail.com>
2018-11-16 07:36:58 +0300

    (chore)add tags migration

--- release.sh
+++ release.sh
@@ -1,5 +1,8 @@
 python manage.py makemigrations authentication
 python manage.py makemigrations profile
 python manage.py makemigrations article
+python manage.py makemigrations rating
+python manage.py makemigrations report
+python manage.py makemigrations tags
 python manage.py makemigrations
 python manage.py migrate
